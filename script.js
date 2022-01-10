6d417dd31e841e119a93016e0742ff48553382c2
ZaikoXander <alexdaniel__@outlook.com>
2022-01-10 17:30:37 -0300

    menus

--- script.js
+++ script.js
@@ -1,58 +1,128 @@
-let menuglobal = [false, false, false, false];
-let nav_menus = document.querySelector('nav.nav-menus');
-let nav_menus_tutoriais = document.querySelector('div.nav-menus-tutoriais');
-let nav_menus_referencias = document.querySelector('div.nav-menus-referencias');
-let nav_menus_exercicios = document.querySelector('div.nav-menus-exercicios');
-let nav_menus_videos = document.querySelector('div.nav-menus-videos');
+let menuglobal = [false, false, false];
+let menu_item = document.getElementsByClassName('menu-item');
+function mouseover_menu_item(index) {
+    menu_item[index].style.backgroundColor = 'rgba(179, 134, 168, 1)';
+    menu_item[index].style.color = 'rgba(255, 251, 238, 1)';
+}
+function mouseout_menu_item(index) {
+    if (menuglobal[index] != true) {
+        menu_item[index].style.backgroundColor = 'rgba(255, 251, 238, 1)';
+        menu_item[index].style.color = 'rgba(179, 134, 168, 1)';
+    }
+}
+
 function fofo_nav(menu) {
+    let nav_menus = document.querySelector('nav.nav-menus');
+    let nav_menus_tutoriais = document.getElementById('tutoriais');
+    let nav_menus_referencias = document.getElementById('referencias');
+    let nav_menus_exercicios = document.getElementById('exercicios');
     let simbolo = document.getElementsByClassName('simbolo');
-    nav_menus.classList.toggle('ativo');
-    switch (menu) {
+
+    /**
+     * Função que reseta os elementos por índice.
+     */
+    function reseta(index1, index2 = null, index3 = null) {
+        let displayNone = [
+            function() {
+                nav_menus_tutoriais.style.display = '';
+            },
+            function() {
+                nav_menus_referencias.style.display = '';
+            },
+            function() {
+                nav_menus_exercicios.style.display = '';
+            }
+        ]
+
+        if (index2 == null && index3 == null) {
+            menu_item[index1].style.backgroundColor = 'rgba(255, 251, 238, 1)';
+            menu_item[index1].style.color = 'rgba(179, 134, 168, 1)';
+            simbolo[index1].classList.replace('fa-caret-up', 'fa-caret-down');
+            displayNone[index1]();
+            menuglobal[index1] = false;
+        } else if (index3 == null) {
+            // Repete o código dentro de eval 3 vezes e adiciona índices de 1 a 2
+            for (let i = 1; i < 3; i++) {
+                eval(
+                    `menu_item[index${i}].style.backgroundColor = 'rgba(255, 251, 238, 1)';
+                    menu_item[index${i}].style.color = 'rgba(179, 134, 168, 1)';
+                    simbolo[index${i}].classList.replace('fa-caret-up', 'fa-caret-down');
+                    displayNone[index${i}]();
+                    menuglobal[index${i}] = false;`
+                )
+            }
+        } else {
+            // Repete o código dentro de eval 3 vezes e adiciona índices de 1 a 3
+            for (let i = 1; i < 4; i++) {
+                eval(
+                    `menu_item[index${i}].style.backgroundColor = 'rgba(255, 251, 238, 1)';
+                    menu_item[index${i}].style.color = 'rgba(179, 134, 168, 1)';
+                    simbolo[index${i}].classList.replace('fa-caret-up', 'fa-caret-down');
+                    displayNone[index${i}]();
+                    menuglobal[index${i}] = false;`
+                )
+            }
+            nav_menus.style.display = '';
+        }
+    }
+
+    /**
+     * O *switch case* abaixo quando algum menu ser chamado irá definir
+     * o menu aberto atualmente como *true* e os outros como *false*,
+     * utiliza a *function reseta*.
+     * Exemplo: Tente abrir o *tutoriais* e logo depois o *referencias*.
+     */
+    switch(menu) {
         case 'tutoriais':
-            nav_menus_tutoriais.classList.toggle('ativo');
+            if (menuglobal[0] == true) {
+                reseta(0, 1, 2);
+            } else {
+                menuglobal[0] = true;
+                reseta(1, 2);
+            }
             break;
         case 'referencias':
-            nav_menus_referencias.classList.toggle('ativo');
+            if (menuglobal[1] == true) {
+                reseta(0, 1, 2);
+            } else {
+                menuglobal[1] = true;
+                reseta(0, 2);
+            }
+            break;
+        case 'exercicios':
+            if (menuglobal[2] == true) {
+                reseta(0, 1, 2);
+            } else {
+                menuglobal[2] = true;
+                reseta(0, 1);
+            }
+            break;
         default:
+            reseta(0, 1, 2);
             break;
     }
-    /*
-    if(!menuglobal.includes(true)) {
+
+    /**
+     * Os *if's* abaixo atualizam as características dos elementos
+     * caso algum menu seja chamado.
+     */
+    if(menuglobal.includes(true)) {
         nav_menus.style.display = 'block';
-        switch (menu) {
-            case 'tutoriais':
-                nav_menus_tutoriais.style.display = 'block';
-                simbolo[0].innerHTML = '&utrif;';
-                menuglobal[0] = true;
-                break;
-            case 'referencias':
-                nav_menus_referencias.style.display = 'block';
-                simbolo[1].innerHTML = '&utrif;';
-                menuglobal[1] = true;
-                break;
-                case 'exercicios':
-                nav_menus_exercicios.style.display = 'block';
-                simbolo[2].innerHTML = '&utrif;';
-                menuglobal[2] = true;
-                break;
-            case 'videos':
-                nav_menus_videos.style.display = 'block';
-                simbolo[3].innerHTML = '&utrif;';
-                menuglobal[3] = true;
-                break;
-            default:
-                break;
+        if (menuglobal[0] == true) {
+            nav_menus_tutoriais.style.display = 'block';
+            menu_item[0].style.backgroundColor = 'rgba(179, 134, 168, 1)';
+            menu_item[0].style.color = 'rgba(255, 251, 238, 1)';
+            simbolo[0].classList.replace('fa-caret-down', 'fa-caret-up');
+        } else if (menuglobal[1] == true) {
+            nav_menus_referencias.style.display = 'block';
+            menu_item[1].style.backgroundColor = 'rgba(179, 134, 168, 1)';
+            menu_item[1].style.color = 'rgba(255, 251, 238, 1)';
+            simbolo[1].classList.replace('fa-caret-down', 'fa-caret-up');
+        } else {
+            nav_menus_exercicios.style.display = 'block';
+            menu_item[2].style.backgroundColor = 'rgba(179, 134, 168, 1)';
+            menu_item[2].style.color = 'rgba(255, 251, 238, 1)';
+            simbolo[2].classList.replace('fa-caret-down', 'fa-caret-up');
         }
-    } else {
-        nav_menus.style.display = '';
-        simbolo[0].innerHTML = '&dtrif;';
-        simbolo[1].innerHTML = '&dtrif;';
-        simbolo[2].innerHTML = '&dtrif;';
-        simbolo[3].innerHTML = '&dtrif;';
-        nav_menus_tutoriais.style.display = '';
-        nav_menus_referencias.style.display = '';
-        nav_menus_exercicios.style.display = '';
-        nav_menus_videos.style.display = '';
     }
-    */
 }
--- test.js
+++
@@ -1,4 +0,0 @@
-let menuglobal = [false, false, false, false];
-for (let index = 0; index < menuglobal.length; index++) {
-    console.log(menuglobal[index]);
-}
